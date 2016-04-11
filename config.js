5ad98a1eb4bb7a4b89cfa10acf39add0ede63ae4
Matthew Schultz <mschultz4@gmail.com>
2016-04-10 21:54:31 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var config = {
     // percent of *X, AX, and A* combos shown
     percent: 12, 
     //duration each stimuli is visible in milliseconds
-    intervalOnScreen: 250, 
+    intervalOnScreen: 300, 
     //duration of break between stimuli in milliseconds
     intervalBreak: 400 
 };
